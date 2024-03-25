2f505cbe4e887be62e80e7a418c028aae2e2b3a7
unknown <GuilhermeWillahelm>
2024-03-25 11:28:33 -0300

    Starting Project

---
+++ NetflixCloneWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetflixCloneWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
